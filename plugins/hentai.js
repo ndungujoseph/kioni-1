822ae6e59de396fbf4a8d5d40aa822ff25862958
Sam Pandey <69663175+sam-pandey@users.noreply.github.com>
2021-05-05 22:42:08 +0530

    Update hentai.js

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -2,7 +2,7 @@
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 Sam Pandey
-
+*/
 
 const Asena = require('../events');
 const axios = require('axios');
@@ -65,4 +65,4 @@ else if (Config.WORKTYPE == 'public') {
 		await message.sendMessage(Buffer.from(drkhent.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by Jarvis' })
 	}));
 }
-*/
+
