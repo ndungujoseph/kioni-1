1d10fd90e856d0c51da98d8557246992b9508884
Sam Pandey <69663175+sam-pandey@users.noreply.github.com>
2021-05-05 16:16:30 +0000

    Update hentai.js

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -1,8 +1,8 @@
 /* Copyright (C) 2021.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-DrkBot - DarkBox1522
-*/
+Sam Pandey
+
 
 const Asena = require('../events');
 const axios = require('axios');
@@ -65,3 +65,4 @@ else if (Config.WORKTYPE == 'public') {
 		await message.sendMessage(Buffer.from(drkhent.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by Jarvis' })
 	}));
 }
+*/
