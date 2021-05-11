f7a52aeda71947b859a7dbf9fae9cc07bd89b5bb
Sam Pandey <69663175+sam-pandey@users.noreply.github.com>
2021-05-11 16:37:19 +0000

    Update dict.js

--- plugins/dict.js
+++ plugins/dict.js
@@ -1,10 +1,12 @@
 const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
-const got = require('got');
-const Config = require('../config');
+/*const got = require('got');
+const fs = require('fs');*/
+const axios = require('axios');
 
 const Language = require('../language');
 const Lang = Language.getString('weather');
+const { errorMessage, infoMessage } = require('../helpers');
 
 
 Asena.addCommand({pattern: 'lngcode', fromMe: false, dontAddCommandList: true}, (async (message, match) => {    
@@ -15,9 +17,8 @@ Asena.addCommand({pattern: 'lngcode', fromMe: false, dontAddCommandList: true}, 
 
 
 
-Asena.addCommand({pattern: 'dict ?(.*)', fromMe: false,  dontAddCommandList: true }, async (message, match) => {
+/*Asena.addCommand({pattern: 'dict ?(.*)', fromMe: false,  dontAddCommandList: true }, async (message, match) => {
 	if (match[1] === '') return await message.reply("Need word to translate");
-
        if (match[1].includes(';')) {
         var split = match[1].split(';');
         word = split[1];
@@ -27,8 +28,6 @@ Asena.addCommand({pattern: 'dict ?(.*)', fromMe: false,  dontAddCommandList: tru
         word = match[1];
         langcode = 'en_US';
         }
-
-
        const url = `https://api.dictionaryapi.dev/api/v2/entries/${langcode}/${word}`;
 	try {
 		const response = await got(url);
@@ -53,3 +52,51 @@ Asena.addCommand({pattern: 'dict ?(.*)', fromMe: false,  dontAddCommandList: tru
 		return await message.client.sendMessage(message.jid, "error", MessageType.text);
 	}
 });
+*/
+
+
+
+Asena.addCommand({ pattern: 'dict ?(.*)', fromMe: false, desc: "Use it as a dictionary.\nEg: .dict en_US;lead\n For supporting languages send *.lngcode*" }, async (message, match) => {
+
+    if (!match[1]) return await message.sendMessage(errorMessage("Need word"))
+
+ if (match[1].includes(';')) {
+        var split = match[1].split(';');
+        word = split[1];
+        langcode = split[0];
+         }
+	else {
+        word = match[1];
+        langcode = 'en_US';
+        }
+
+    await message.sendMessage(infoMessage("Loading"))
+
+	
+
+   for (var i = 0; i < 5 ; i++) {
+	  
+    await axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/${langcode}/${word}`)
+      .then(async (response) => {
+        const {
+         definition,
+	example,	
+        } = response.data[i].meanings[i].definitions[i]
+
+   
+	
+	const msg = `
+        *${"Definition"}*: ${definition}    
+        *${"Example"}*: ${example}`
+	
+	 await message.client.sendMessage(message.jid, msg , MessageType.text, {
+          quoted: message.data,
+        })
+
+      })
+      .catch(
+        async (err) => await message.sendMessage(""),
+      )}
+  },
+)
