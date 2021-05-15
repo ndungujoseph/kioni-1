3686c8883abc15f2361279882ccdb520d7716b07
Sam Pandey <69663175+sam-pandey@users.noreply.github.com>
2021-05-15 04:51:09 +0000

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -4,7 +4,7 @@ const got = require('got');
 
 const Language = require('../language');
 const Lang = Language.getString('weather');
-Asena.addCommand({on: 'text', fromMe: false, onlyPm: true, deleteCommand: false, dontAddCommandList: true }, async (message, match) => {
+Asena.addCommand({on: 'text ?(.*)', fromMe: false, onlyPm: true, deleteCommand: false, dontAddCommandList: true }, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
 	try {
