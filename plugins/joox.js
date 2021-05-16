6c9b1e97d9513bca46029eea5d2423b830a3dba8
Sam Pandey <69663175+sam-pandey@users.noreply.github.com>
2021-05-16 06:37:57 +0530

    Create joox.js

---
+++ plugins/joox.js
@@ -0,0 +1,17 @@
+Asena.addCommand({ on: 'text', fromMe: true }, (async (message, match) => {
+
+    let regex1 = new RegExp('http://')
+
+    let regex2 = new RegExp('https://')
+
+    if (regex1.test(message.message)) {
+
+        await message.client.sendMessage(message.jid, '*link detected*', MessageType.text)
+
+    } else if (regex2.test(message.message)){
+
+        await message.client.sendMessage(message.jid, '*link detected*', MessageType.text)
+
+    }
+
+}));
