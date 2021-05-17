ddf3c5522b43244f16abfbee2488f7c928ece124
abdurehmanov <81478528+abdurehmanov@users.noreply.github.com>
2021-05-17 10:00:34 +0400

    Update admin.js

--- plugins/admin.js
+++ plugins/admin.js
@@ -40,7 +40,7 @@ Asena.addCommand({pattern: 'ban ?(.*)', fromMe: true, onlyGroup: true, desc: Lan
                 message.jid, 
                 fs.readFileSync("/root/WhatsAsenaDuplicated/media/gif/Edited_20210227_152650.mp4"),
                 MessageType.video, 
-                { mimetype: Mimetype.gif, caption: "Banned by Jarvis" }
+                { mimetype: Mimetype.gif, caption: "Banned by Admin" }
             )
             await message.client.sendMessage(message.jid,'```Haha Loser,Get lost from here``` ' +'@' + message.reply_message.data.participant.split("@")[0] + ' 😈', MessageType.text, {contextInfo: {mentionedJid: [message.reply_message.data.participant]}});
             await message.client.groupRemove(message.jid, [message.reply_message.data.participant]);
