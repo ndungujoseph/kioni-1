a111dd2a60021472c4757a518402fac60123293f
ABDUREHMANOFFFF <81478528+abdurehmanoff@users.noreply.github.com>
2021-05-17 17:10:01 +0400

    Update Xmeadia.js

--- plugins/Xmeadia.js
+++ plugins/Xmeadia.js
@@ -59,7 +59,7 @@ if (Config.WORKTYPE == 'private') {
             .format('mp4')
             .save('output.mp4')
             .on('end', async () => {
-                await message.sendMessage(fs.readFileSync('output.mp4'), MessageType.video, {caption: 'Made for Founder'});
+                await message.sendMessage(fs.readFileSync('output.mp4'), MessageType.video, {caption: 'Made for Owner'});
             });
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));
@@ -80,7 +80,7 @@ if (Config.WORKTYPE == 'private') {
             .format('mp4')
             .save('output.mp4')
             .on('end', async () => {
-                await message.sendMessage(fs.readFileSync('output.mp4'), MessageType.video, {caption: 'Made for Founder'});
+                await message.sendMessage(fs.readFileSync('output.mp4'), MessageType.video, {caption: 'Made for Owner'});
             });
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));
