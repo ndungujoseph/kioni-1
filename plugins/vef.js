b94df15d32a027997dcdbd5463855e542ba5bae0
Abdurehman <81478528+abdurehmanoff@users.noreply.github.com>
2021-05-21 14:42:41 +0400

    Update vef.js

--- plugins/vef.js
+++ plugins/vef.js
@@ -35,6 +35,15 @@ Asena.addCommand({pattern: 'vef ?(.*)', fromMe: true, desc: TRvefdesc}, (async (
            { mimetype: Mimetype.mp4Audio, ptt: true}
         )
     }
+    else if (match[1] === 'kalbimiz kırıldı' || match[1] === 'kurumuş boğazım' || match[1] === 'saidojasoidjasodjasoidjsa') {
+
+        await message.client.sendMessage(
+            message.jid, 
+            fs.readFileSync("/root/WhatsAsenaDuplicated/media/ses/4_5900017439466326353.mp3"),
+            MessageType.audio, 
+           { mimetype: Mimetype.mp4Audio, ptt: true}
+        )
+    }
     else if (match[1] === 'yasam' || match[1] === 'yasam masam' || match[1] === 'yaşam') {
 
         await message.client.sendMessage(
@@ -155,7 +164,7 @@ Asena.addCommand({pattern: 'vef ?(.*)', fromMe: true, desc: TRvefdesc}, (async (
     else {
         await message.client.sendMessage(
             message.jid, 
-            '```Bulunamadı! Varolan Liste:```\n\n$onun bunun / bende türküm\n$hadsiz / hadsız\n$yasam / yasam masam\n$yavas / yavas git\n$noluyo / noluyo lan\n$kardeslik / kardes\n$yalan / namik kemal\n$bursa cocugu / bursa\n$hakkimi / helal\n$allah ya / geldi/\n$/fehleleruy/ biz\n$/mehemmed deniz/allahverdi/atam balasi/deniz\n$/seni hic alakadar etmez/memati/alakadar/alakdar etmez\n$/kavdesim/ kavdesim helikopter\n$/oh no/ no',
+            '```Bulunamadı! Varolan Liste:```\n\n$onun bunun / bende türküm\n$hadsiz / hadsız\n$yasam / yasam masam\n$yavas / yavas git\n$noluyo / noluyo lan\n$kardeslik / kardes\n$yalan / namik kemal\n$bursa cocugu / bursa\n$hakkimi / helal\n$allah ya / geldi/\n$/fehleleruy/ biz\n$/mehemmed deniz/allahverdi/atam balasi/deniz\n$/seni hic alakadar etmez/memati/alakadar/alakdar etmez\n$/kavdesim/ kavdesim helikopter\n$/oh no/ no\n$/kurumuş boğazım/ kalbimiz kırıldı',
             MessageType.text
         )
     }
