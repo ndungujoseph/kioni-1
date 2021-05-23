a3063f6b54d6244b6870c201fe2e53278c279725
KioniNdungu <84273819+KioniNdungu@users.noreply.github.com>
2021-05-23 18:29:41 +0300

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -1,3 +1,11 @@
+/* Copyright (C) 2020 Yusuf Usta.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+WhatsAsena - Yusuf Usta
+*/
+
 const Asena = require('../events');
 const Config = require('../config');
 const {MessageType} = require('@adiwajshing/baileys');
@@ -10,9 +18,9 @@ Asena.addCommand({pattern: 'asena ?(.*)', fromMe: true, dontAddCommandList: true
     if (match[1] === '') {
         Asena.commands.map(
             async (command) =>  {
-                if (command.dontAddCommandList || command.pattern === undefined) return;
+                if (command.dontAddCommandList || command.pattern === undefined) return;
                 try {
-                    var match = command.pattern.toString().match(/(\W*)([A-Za-züşıiğöç1234567890 ]*)/);
+                    var match = command.pattern.toString().match(/(\W*)([A-Za-zğüşiöç]*)/);
                 } catch {
                     var match = [command.pattern];
                 }
@@ -24,44 +32,22 @@ Asena.addCommand({pattern: 'asena ?(.*)', fromMe: true, dontAddCommandList: true
                 } else {
                     HANDLER = '.';
                 }
-                
-                if (command.desc == '' && !command.usage == '' && command.warn == '') {
-                    CMD_HELP += '*🛠 ' + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + '```\n' + '*⌨️ ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
-                }
-                if (!command.desc == '' && command.usage == '' && command.warn == '') {
-                    CMD_HELP += '*🛠 ' + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + '```\n' + '*💬 ' + Lang.DESC + ':* ```' + command.desc + '``` \n\n';
-                }
-                if (command.desc == '' && command.usage == '' && !command.warn == '') {
-                    CMD_HELP += '*🛠 ' + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + '```\n' + '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
-                }
-                if (!command.desc == '' && !command.usage == '' && command.warn == '') {
-                    CMD_HELP += '*🛠 ' + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + '```\n' + '*💬 ' + Lang.DESC + ':* ```' + command.desc + '``` \n' + '*⌨️ ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
-                }
-                if (!command.desc == '' && command.usage == '' && !command.warn == '') {
-                    CMD_HELP += '*🛠 ' + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + '```\n' + '*💬 ' + Lang.DESC + ':* ```' + command.desc + '``` \n' + '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
-                }
-                if (command.desc == '' && !command.usage == '' && !command.warn == '') {
-                    CMD_HELP += '*🛠 ' + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + '```\n' + '*⌨️ ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n' + '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
-                }
-                if  (command.desc == '' && command.usage == '' && command.warn == '') {
-                    CMD_HELP += '*🛠 ' + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + '```\n\n'
-                }
-                if  (!command.desc == '' && !command.usage == '' && !command.warn == '') {
-                    CMD_HELP += '*🛠 ' + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + '```\n' + '*💬 ' + Lang.DESC + ':* ```' + command.desc + '``` \n' + '*⌨️ ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n' + '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n' 
-                }
+                CMD_HELP += '*🛠 ' + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + (command.desc === '' ? '```\n\n' : '```\n');
+                if (command.desc !== '') CMD_HELP += '*💬 ' + Lang.DESC + ':* ```' + command.desc + (command.usage === '' ? '```\n\n' : '```\n');
+                if (command.usage !== '') CMD_HELP += '*⌨️ ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
             }
         );
-        return await message.client.sendMessage(
-            message.jid,'●▬▬▬ *Kioni Ndungu Edition* ▬▬▬●\n\n' + CMD_HELP, MessageType.text
-        );    
         
+        await message.client.sendMessage(
+            message.jid, CMD_HELP, MessageType.text
+        );    
     } else {
-        var CMD = '';
+        var CMD_HELP = '';
         Asena.commands.map(
             async (command) =>  {
-                if (command.dontAddCommandList || command.pattern === undefined) return;
+                if (command.dontAddCommandList || command.pattern === undefined) return;
                 try {
-                    var cmatch = command.pattern.toString().match(/(\W*)([A-Za-züşğiöç 1234567890]*)/);
+                    var cmatch = command.pattern.toString().match(/(\W*)([A-Za-zğüşiöç]*)/);
                 } catch {
                     var cmatch = [command.pattern];
                 }
@@ -74,17 +60,15 @@ Asena.addCommand({pattern: 'asena ?(.*)', fromMe: true, dontAddCommandList: true
                     } else {
                         HANDLER = '.';
                     }
-                    CMD += '*🛠 ' + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmatch[2]) : command.pattern) + (command.desc === '' ? '```\n\n' : '```\n');
-                    if (command.desc !== '') CMD += '*💬 ' + Lang.DESC + ':* ```' + command.desc + (command.warn === '' ? '```\n\n' : '```\n');
-                    if (command.usage !== '') CMD += '*⌨️ ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
-                    if (command.warn !== '') CMD += '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n';
-
+                    CMD_HELP += '*🛠 ' + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmatch[2]) : command.pattern) + (command.desc === '' ? '```\n\n' : '```\n');
+                    if (command.desc !== '') CMD_HELP += '*💬 ' + Lang.DESC + ':* ```' + command.desc + (command.usage === '' ? '```\n\n' : '```\n');
+                    if (command.usage !== '') CMD_HELP += '*⌨️ ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';    
                 }
             }
         );
-        if (CMD == '') CMD += Lang.NOT_FOUND;
+        if (CMD_HELP === '') CMD_HELP += Lang.NOT_FOUND;
         await message.client.sendMessage(
-            message.jid,'●▬▬▬ *Kioni Ndungu   Edition* ▬▬▬●\n\n' + CMD, MessageType.text
+            message.jid, CMD_HELP, MessageType.text
         );
     }
 }));
